58a42a81ae1d4845529672c7db8870c9695ff204
Partha Pratim Das <dasparthapratim02@gmail.com>
2022-02-24 23:43:28 +0530

    update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,8 +3,8 @@ const User = require("../models/user.model")
 const { body, validationResult } = require('express-validator');
 const router = express.Router()
 
-router.post("", body("first_name").notEmpty().isLength({ min: 2 }, { max: 35 }),
-    body("last_name").notEmpty().isLength({ min: 2 }, { max: 35 }),
+router.post("", body("name").notEmpty().isLength({ min: 2 }, { max: 35 }),
+    body("password").notEmpty().isLength({ min: 2 }, { max: 8 }),
     body("mobile").isNumeric().notEmpty().isLength({ min: 10 }, { max: 10 }),
     body("email").notEmpty().custom(async value => {
         const user = await User.findOne({email: value})
@@ -21,7 +21,7 @@ router.post("", body("first_name").notEmpty().isLength({ min: 2 }, { max: 35 }),
               return res.status(400).json({ errors: errors.array() });
             }
             const user = await User.create(req.body)
-            return res.render ("")
+            return res.send (user)
     } catch (e) {
         console.log(e.message)
     }

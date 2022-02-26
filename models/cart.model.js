0acdac0ab6118d8631a6b69b678d25104f0ae5ea
Partha Pratim Das <dasparthapratim02@gmail.com>
2022-02-26 11:23:45 +0530

    updt

--- models/cart.model.js
+++ models/cart.model.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose")
 
 const cartSchema = new mongoose.Schema({
     serialNo:{ type: Number, require: true },
-    product_url:{ type: String, require: true },
-    product_name: { type: String, require: true }, 
+    // product_url:{ type: String, require: true },
+    // product_name: { type: String, require: true }, 
     quantity: { type: Number, require: true },
     pay: { type: Number, require: true },
     productId1: [{ type: mongoose.Schema.Types.ObjectId, ref: "fruit",require: false }],
